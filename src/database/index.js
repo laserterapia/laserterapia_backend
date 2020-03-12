357285fe6758565b20a962dc14b428b59f9f7dbd
Agnaldo <agnaldo.souto.junior@accenture.com>
2020-03-12 14:42:55 -0300

    ajuste db

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const { user, password } = require("../config/mongo.json");
 
-mongoose.connect('mongodb://localhost/noderest', { useNewUrlParser: true })
-mongoose.Promise = global.Promise
+const uri = `mongodb+srv://${user}:${password}@clusterlaserterapia-uastv.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("SUCESSO"))
+  .catch(error => console.log(error));
+
+mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
